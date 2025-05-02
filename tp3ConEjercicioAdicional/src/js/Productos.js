12992bc7a4fadeea469fa99586921110c1472b35
Matias-Mamni5315 <matiasmamani998@gmail.com>
2025-05-02 19:21:58 -0300

    se creo la carpeta js dentro de src donde se cargo el archivo Productos.js en donde se cargo un array de productos necesarios para el ejercicio adicional.

---
+++ tp3ConEjercicioAdicional/src/js/Productos.js
@@ -0,0 +1,7 @@
+const productos = [
+    { descripcion: "Teclado", precio: 150000.75 },
+    { descripcion: "Mouse Inalámbrico", precio: 15000.50 },
+    { descripcion: "Auriculares Gamer", precio: 102315.99 },
+    { descripcion: "Monitor", precio: 115750.25 },
+    { descripcion: "Cámara", precio: 90000.45 }
+]
