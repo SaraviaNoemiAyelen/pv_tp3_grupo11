93aea33f245c7a4bef883f916311fcee7f43f52a
Isabelafk <mendoza9848248@gmail.com>
2025-05-02 19:46:14 -0300

    Se agrego en el archivo ejerciciosAdicionales.js el punto 3 que se nos pedia crear un array con los productos incluyendo en el precio el IVA

--- tp3ConEjercicioAdicional/src/js/ejerciciosAdicionales.js
+++ tp3ConEjercicioAdicional/src/js/ejerciciosAdicionales.js
@@ -11,3 +11,11 @@ export const filtrarMayorA20 = (productos) => {
     const productosMayorA20 = productos.filter(prod => prod.precio > 20000);
     console.log(productosMayorA20);
 }
+//3 - Crear un array con los productos, pero con el precio con IVA incluido (21%), usando map.
+export const precioConIVA = (productos) => {
+    const precioModificadoConIVA = productos.map(prod => ({
+        descripcion: prod.descripcion,
+        precio: (prod.precio * 1.21).toFixed(2)
+    }));
+    console.log(precioModificadoConIVA);
+}
